73f70dc63702327ac4beedea647962c6b3e2fee9
hamitov renat <hamitov@live.ru>
2022-11-02 11:11:37 +0300

    Add streamlit

--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,19 @@
 from transformers import pipeline
+import io
+import streamlit as st
+from PIL import Image
 classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
-classifier("Я обожаю инженерию машинного обучения!")
+
+
+
+st.title('Тональность текста')
+inp_text = st.text('Введите текст')
+
+result = st.button('Определить тональность')
+
+if result:
+    c = classifier(inp_text)
+    st.write(str(c))
+	#st.write(str(c))
+    
+    

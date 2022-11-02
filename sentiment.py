14e7960715fe8c5ea800177d09521688c40d1e4c
hamitov renat <hamitov@live.ru>
2022-11-02 11:45:09 +0300

    modified:   sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -15,7 +15,7 @@ result = st.button('Определить тональность')
 if result:
     cl = classifier(str(inp_text))
     for i in cl:
-        st.write(str(i))
+        st.write((str(i["label"]),' с вероятностью '))
         #st.write((str(i["label"]),' с вероятностью ',str(100*float(i["score"])),'%')
 
     
